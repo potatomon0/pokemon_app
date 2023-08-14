0eb33ce8132c557afb93ad438efcf47af01991d9
Hu <huang.h0709@gmail.com>
2023-08-14 10:16:48 -0400

    My server is set up and runnning

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/',(req,res)=>{
+    res.send('Welcome to the Pokemon App!')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,(req,res)=>{
+    console.log('Listening to 3k')
+})
